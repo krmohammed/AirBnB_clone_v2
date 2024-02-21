a6d52104fec0684104b8cfc0fd48a16007f7aefc
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-02-21 14:55:59 +0000

    stateless

--- models/state.py
+++ models/state.py
@@ -15,10 +15,10 @@ class State(BaseModel):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
 
-    #if getenv("HBNB_TYPE_STORAGE") != "db":
-    @property
-    def cities(self):
-        """getter -> cities"""
-        cityL = [city for city in list(models.storage.all(City).values())
-                 if city.state_id == self.id]
-        return cityL
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """getter -> cities"""
+            cityL = [city for city in list(models.storage.all(City).values())
+                     if city.state_id == self.id]
+            return cityL
