27c79309dd3766c6a3b0dbe696ce00af3e706732
Mohammed Khalilu Rahman <mohrahman558@gmail.com>
2024-05-04 04:20:04 +0000

    task 1 improved

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,3 +1,23 @@
-#!/usr/bin/env bash
-# fabric script that generates a .tgz archive
+#!/usr/bin/python3
+""" fabric script that generates a .tgz archive """
+import os
+from fabric.api import local
+from datetime import datetime
 
+
+def do_pack():
+    """
+    generates .tgz file from the contents of the web_static folder
+    """
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
+
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(now)
+
+    tgz_file = local("tar -cvzf {} web_static".format(filename))
+
+    if tgz_file.succeeded:
+        return tgz_file
+    else:
+        return None
