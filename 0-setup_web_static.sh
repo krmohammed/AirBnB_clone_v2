f86c75fc48ba0ecda963e6f91d9dd2576f49ec9c
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-04 03:18:54 +0000

    preparing web server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# sets up a web server for deployment
+
+if ! dpkg -l | grep nginx > /dev/null; then
+	apt-get update
+	apt-get install -y nginx
+fi
+
+mkdir -p /data/web_static/{releases/test, shared}
+
+echo "<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+	<h1>Holberton School</h1>
+</body>
+</html>" | tee /data/web_static/releases/test/index.html > /dev/null
+
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chmod -R ubuntu:ubuntu /data
+
+sed -i '/^server {/a \\n\tlocation hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+service nginx restart
