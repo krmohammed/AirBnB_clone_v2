dbae96c4ce54477925d714b288d6b49b6c47e235
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-23 12:10:01 +0000

    task 0 and 1

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" starts a flask web app """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_hbtn():
+    """ hello_hbtn route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ hbnb route """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
