afc21c8424a271a4fa2ce12cc364788a94d04cf4
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-23 21:05:04 +0000

    task 3 fix

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,9 +25,9 @@ def c_route(text):
     return f"C {text}"
 
 
-@app.route("/python/", defaults={"text": "is fun"}, strict_slashes=False)
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
+def python(text):
     """ python route """
     text = text.replace("_", " ")
     return f"Python {text}"
