54e67c0bead8bbe0ccd895cae6ecb3398c5c9806
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-23 20:51:18 +0000

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,10 @@ def hbnb():
 
 
 @app.route("/c/<text>", strict_slashes=False)
-def c_route()
+def c_route(text):
+    """ c route """
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
