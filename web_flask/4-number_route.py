89880bfcc79fbeca7da37ea68a608520445af8db
Khalilu Rahman Mohammed <mohrahman558@gmail.com>
2024-05-23 21:13:43 +0000

    n is a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" starts a flask web app """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbtn():
+    """ hello_hbtn route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ hbnb route """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """ c route """
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    """ python route """
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """ number route """
+    return f"{n} is a number"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
